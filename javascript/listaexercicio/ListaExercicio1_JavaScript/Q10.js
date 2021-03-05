28b448821a032822ec6ae2fae53d3da69eb7cb53
GabrielGui13 <gabrielguilherme13@hotmail.com>
2021-03-05 00:15:19 -0300

    q7 e q10

---
+++ javascript/listaexercicio/ListaExercicio1_JavaScript/Q10.js
@@ -0,0 +1,9 @@
+function checarInteiro (x) {
+    if (x % 3 == 0) return true
+    else return false
+}
+
+console.log(checarInteiro(3))
+console.log(checarInteiro(4))
+console.log(checarInteiro(30))
+console.log(checarInteiro(40))
--- javascript/listaexercicio/ListaExercicio1_JavaScript/Q7.js
+++ javascript/listaexercicio/ListaExercicio1_JavaScript/Q7.js
@@ -1,5 +1,19 @@
-function equacao (a, b, c) {
+function equacao (a = 0, b = 0, c = 0) {
+    const resultado = []
+    let delta = Math.sqrt(Math.pow(b, 2) - 4*a*c)
 
+
+    let raizPos = (-b + delta) / (2*a)
+    let raizNeg = (-b - delta) / (2*a)
+
+    if (isNaN(delta)) {
+        return "Delta eh negativo"
+    } else {
+        resultado.push(raizPos.toFixed(2), raizNeg.toFixed(2))
+        return resultado
+    }
 }
 
-console.log(3, -15, 2)
+console.log(equacao(3, -15, 2))
+console.log(equacao(4, 2, -6))
+console.log(equacao(7, 3, 4))
