3397bc800e658c13948a9565e921ce9e8738e979
GabrielGui13 <gabrielguilherme13@hotmail.com>
2021-03-11 00:53:41 -0300

    inicio do modulo objeto

---
+++ javascript/objeto/oo.js
@@ -0,0 +1,22 @@
+// CODIGO NAO EXECUTAVEL !!
+
+// Procedural
+processamento(valor1 ,valor2, valor3)
+
+// OO
+objeto = [
+    valor1,
+    valor2,
+    valor3,
+    processamento() {
+        // ...
+    }
+]
+
+objeto.processamento() //funciona como capsula que agrupa comportamentos e atributos, foco passou a ser o objeto
+
+//Principios importantes
+//1. abstracao
+//2. Encapsulamento
+//3. Herança (prototype)
+//4. Polimorfismo
