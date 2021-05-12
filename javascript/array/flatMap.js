62e486d30252e2c835dbf90f507cb3f1cad522e4
GabrielGui13 <gabrielguilherme13@hotmail.com>
2021-05-11 23:38:07 -0300

    finalizacao do modulo

--- javascript/array/flatMap.js
+++ javascript/array/flatMap.js
@@ -1,4 +1,37 @@
 const escola = [{
-    nome: 'Escola',
-
+    nome: 'Turma M1',
+    alunos: [{
+        nome: 'Gustavo',
+        nota: 8.1
+    }, {
+        nome: 'Ana',
+        nota: 9.3
+    }]
+}, {
+    nome: 'Turma M2',
+    alunos: [{
+        nome: 'Rebeca',
+        nota: 8.9
+    },  {
+        nome: 'Roberto',
+        nota: 7.3
+    }]
 }]
+
+const getNotaDoAluno = aluno => aluno.nota
+const getNotasDaTurma = turma => turma.alunos.map(getNotaDoAluno)
+
+const notas1 = escola.map(getNotasDaTurma)
+console.log(notas1)
+
+//Criando o flatMap da epoca
+Array.prototype.flatMap2 = function(callback) {
+    return Array.prototype.concat.apply([], this.map(callback))
+} 
+const notas2 = escola.flatMap2(getNotasDaTurma)
+console.log(notas2) 
+
+
+//Hoje ele ja existe
+const notas3 = escola.flatMap(getNotasDaTurma)
+console.log(notas3)
